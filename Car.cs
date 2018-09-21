6b3ea810ffb872b6f0c2d61fa9c9e31c721e87fb
SD18 <SD18@mitt.ca>
2018-09-21 09:50:56 -0500

    initial commit

--- Car.cs
+++ Car.cs
@@ -7,5 +7,20 @@ namespace MVCassignment1
 {
     public class Car
     {
+        public int Cost { get; set; }
+        public int Year { get; set; }
+        public int Make { get; set; }
+        public int Model { get; set; }
+        public string Color { get; set; }
+
+        public Car(int cost, int year, int make, int model, string color)
+        {
+            Cost = cost;
+            Year = year;
+            Make = make;
+            Model = model;
+            Color = color;
+
+        }
     }
 }
